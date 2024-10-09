8875d2e50bd8219224546330d3bc22f6f1c0f5fb
mshahcode <mikayil.shahtakhtinski@gmail.com>
2024-10-08 23:58:33 -1100

    added glab 303_5_2

---
+++ Module-303/Module-303/src/homework/M303_5/GLAB_M303_5_2.java
@@ -0,0 +1,30 @@
+package homework.M303_5;
+
+
+import java.util.Scanner;
+
+
+
+class GuesstheNumber {
+    public static void main(String[] args) {
+        // Generate a random number to be guessed
+        int number = (int) (Math.random() * 101);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a magic number between 0 and 100");
+        int guess = -1;
+        while (guess != number) {
+            // Prompt the user to guess the number
+            System.out.print("\nEnter your guess: ");
+            guess = input.nextInt();
+            if (guess == number)
+                System.out.println("Yes, the number is " + number);
+            else if (guess > number)
+                System.out.println("Your guess is too high");
+            else
+                System.out.println("Your guess is too low");
+        } // End of loop
+    }
+}
+
+public class GLAB_M303_5_2 {
+}
