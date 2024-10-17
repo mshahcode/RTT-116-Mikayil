b7823cd6ad63460c88953c42c4996e12bc084566
mshahcode <mikayil.shahtakhtinski@gmail.com>
2024-10-16 14:25:37 -1100

    added more 303_10 pa

--- Module-303/Module-303/src/homework/M303_10/PA_303_10_2.java
+++ Module-303/Module-303/src/homework/M303_10/PA_303_10_2.java
@@ -1,4 +1,12 @@
 package homework.M303_10;
 
+abstract class Book{
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+}
+
 public class PA_303_10_2 {
 }
